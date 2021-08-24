86bffe8164b7a677bc630741e812a941c073bf70
Naima Hassan <naima.hassan434@gmail.com>
2021-08-24 22:25:33 +0100

    Rendering CSS, Images, SVG files

--- index.js
+++ index.js
@@ -21,13 +21,22 @@ const server = http.createServer(async (req, res) => {
     const html = await fs.readFile("./views/overview.html", "utf-8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
+  } else if (/\.(png)$/i.test(req.url)) {
+    const image = await fs.readFile(`./public/images/${req.url.slice(1)}`);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    res.end(image);
+  } else if (/\.(css)$/i.test(req.url)) {
+    const css = await fs.readFile(`./public/css/index.css`);
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(css);
+  } else if (/\.(svg)$/i.test(req.url)) {
+    const svg = await fs.readFile(`./public/images/icons.svg`);
+    res.writeHead(200, { "Content-Type": "image/svg+xml" });
+    res.end(svg);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<div><h1>File Not Found</h1></div>");
   }
-
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.end("<h1>Welcome to the Bicycle Shop</h1>");
 });
 
 server.listen(3000);
