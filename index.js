1bcff8e17f7ee395fddca658274ac770eeb9d341
Naima Hassan <naima.hassan434@gmail.com>
2021-08-24 20:46:18 +0100

    Declaring Routes and Rendering HTML

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-//Server create
-
+//Modules
 const http = require("http");
 const url = require("url");
+const fs = require("fs").promises;
 
-const server = http.createServer((req, res) => {
+//Server create
+
+const server = http.createServer(async (req, res) => {
   if (req.url === "/favicon.ico") return;
-  console.log(req.headers);
 
+  //Parsing URL
   const myUrl = new URL(req.url, "http://${req.headers.host}/");
   const pathname = myUrl.pathname;
   const id = myUrl.searchParams.get("id");
 
-  console.log(pathname, id);
+  if (pathname === "/") {
+    const html = await fs.readFile("./views/bicycles.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else if (pathname === "/bicycle" && id >= 0 && id <= 5) {
+    const html = await fs.readFile("./views/overview.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<div><h1>File Not Found</h1></div>");
+  }
 
-  //   console.log(myUrl);
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Welcome to the Bicycle Shop</h1>");
+  //   res.writeHead(200, { "Content-Type": "text/html" });
+  //   res.end("<h1>Welcome to the Bicycle Shop</h1>");
 });
 
 server.listen(3000);
