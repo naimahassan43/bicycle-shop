52b5597a37f77db6e2f77302227934b520cfa445
Naima Hassan <naima.hassan434@gmail.com>
2021-08-24 18:59:35 +0100

    npm packages install and sending response to server

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log("Server running");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Welcome to the Bicycle Shop</h1>");
 });
 
 server.listen(3000);
