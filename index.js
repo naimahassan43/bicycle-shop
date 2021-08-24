09d57910b4a1501e83c8161f715dc62789d72188
Naima Hassan <naima.hassan434@gmail.com>
2021-08-24 22:56:58 +0100

    Rendering Dynamic data for Overview Page

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs").promises;
 
+const bicycles = require("./data/data.json");
+
 //Server create
 
 const server = http.createServer(async (req, res) => {
@@ -13,15 +15,35 @@ const server = http.createServer(async (req, res) => {
   const pathname = myUrl.pathname;
   const id = myUrl.searchParams.get("id");
 
+  // Routes
+  // Home page
   if (pathname === "/") {
     const html = await fs.readFile("./views/bicycles.html", "utf-8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
-  } else if (pathname === "/bicycle" && id >= 0 && id <= 5) {
-    const html = await fs.readFile("./views/overview.html", "utf-8");
+  }
+
+  // Overview Page
+  else if (pathname === "/bicycle" && id >= 0 && id <= 5) {
+    let html = await fs.readFile("./views/overview.html", "utf-8");
+    const bicycle = bicycles.find((b) => b.id === id);
+
+    html = html.replace(/<%IMAGE%>/g, bicycle.image);
+    html = html.replace(/<%NAME%>/g, bicycle.name);
+
+    let price = bicycle.originalPrice;
+    if (bicycle.hasDiscount) {
+      price = (price * (100 - bicycle.discount)) / 100;
+    }
+
+    html = html.replace(/<%NEWPRICE%>/g, `$${price}`);
+
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
-  } else if (/\.(png)$/i.test(req.url)) {
+  }
+
+  //Images
+  else if (/\.(png)$/i.test(req.url)) {
     const image = await fs.readFile(`./public/images/${req.url.slice(1)}`);
     res.writeHead(200, { "Content-Type": "image/png" });
     res.end(image);
