b06c30c7e0d60ad460ae6664ab2f0070ecf656fb
Naima Hassan <naima.hassan434@gmail.com>
2021-08-24 19:20:42 +0100

    Parsing URL and Query String

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 //Server create
 
 const http = require("http");
+const url = require("url");
+
 const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return;
+  console.log(req.headers);
+
+  const myUrl = new URL(req.url, "http://${req.headers.host}/");
+  const pathname = myUrl.pathname;
+  const id = myUrl.searchParams.get("id");
+
+  console.log(pathname, id);
+
+  //   console.log(myUrl);
+
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Welcome to the Bicycle Shop</h1>");
 });
